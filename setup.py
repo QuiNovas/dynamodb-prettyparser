f4e707528c95bff5a636969580646494fd232207
Mathew Moon <mmoon@quinovas.com>
2020-08-17 13:27:22 -0500

    Fix homepage

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='dynamodb-prettyparser',
-    version='1.0.0',
+    version='1.0.1',
     description='Parses dynamodb responses into a list dictionaries with attribute names as keys',
     author='Mathew Moon',
     author_email='mmoon@quinovas.com',
