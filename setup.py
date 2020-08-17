68ddccfad390e74baa977100b21250be8c05fdca
Mathew Moon <mmoon@quinovas.com>
2020-08-17 13:27:06 -0500

    Fix homepage

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Parses dynamodb responses into a list dictionaries with attribute names as keys',
     author='Mathew Moon',
     author_email='mmoon@quinovas.com',
-    url='https://github.com/QuiNovas/aurora-prettyparser',
+    url='https://github.com/QuiNovas/dynamodb-prettyparser',
     license='Apache 2.0',
     long_description=io.open('README.rst', encoding='utf-8').read(),
     long_description_content_type='text/x-rst',
